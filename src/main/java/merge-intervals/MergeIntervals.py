f065ff572ae492e44a0833765aaeb1ef1ce127ed
nongcobjhb024 <nongcobjhb024@student.wethinkcode.co.za>
2025-10-16 21:54:31 +0200

    updated some error handling

--- src/main/java/merge-intervals/MergeIntervals.py
+++ src/main/java/merge-intervals/MergeIntervals.py
@@ -2,22 +2,22 @@ def merge_intervals(interval):
     new_list = []
     initial = 0
     for i in interval:
-        if i[0] > initial:
-            _lst = []
-            initial = i[1]
-            _lst = i
-            new_list.append(_lst)
-            continue
-        else:
-            list = []
-            list.append(_lst[0])
-            list.append(i[1])
-            new_list.pop()
-            new_list.append(list)
-           
-            
+        if len(i) > 0 :
+            if i[0] > initial:
+                _lst = []
+                initial = i[1]
+                _lst = i
+                new_list.append(_lst)
+                continue
+            else:
+                _list = []
+                list.append(_lst[0])
+                list.append(i[1])
+                new_list.pop()
+                new_list.append(_list)
+    
         
         
     return new_list
 
-# print(merge_intervals( [[1,4],[4,5]]))
+print(merge_intervals([[]]))
